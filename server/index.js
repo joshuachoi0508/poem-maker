3ff02b516e569713c648c020a9654f2e159dfb2e
Josh Choi <joshuachoi0508@gmail.com>
2019-06-05 10:58:22 -0700

    Set up post route

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(bodyParser.json);
 app.use(cors());
 
+const posts = require('./routes/api/posts');
+
+app.use('/api/posts', posts)
+
 const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
