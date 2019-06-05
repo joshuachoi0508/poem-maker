5bd357b16974013c28f37197c7153af1ed562114
Josh Choi <joshuachoi0508@gmail.com>
2019-06-05 16:35:33 -0700

    Create post, delete, and get routes

--- server/routes/api/poems.js
+++ server/routes/api/poems.js
@@ -5,15 +5,29 @@ const router = express.Router();
 
 // Get poems
 router.get('/', async (req, res) => {
-  const poems = await loadPoemssCollection();
+  const poems = await loadPoemsCollection();
   res.send(await poems.find({}).toArray());
 })
 
 // Add poem
+router.post('/', async(req, res) => {
+  const poems = await loadPoemsCollection();
+  await poems.insertOne({
+    text: req.body.text,
+    createdAt: new Date()
+  })
+
+  res.status(201).send();
+})
 
 // Delete poem
+router.delete('/:id', async(req, res) => {
+  const poems = await loadPoemsCollection();
+  await poems.deleteMany({_id: new mongodb.ObjectID(req.params.id)});
+  res.status(200).send();
+});
 
-async function loadPoemssCollection() {
+async function loadPoemsCollection() {
   const client = await mongodb.MongoClient.connect('mongodb://poem_creator:poem_creator123@ds133137.mlab.com:33137/poem_creator', {
     useNewUrlParser: true
   });
