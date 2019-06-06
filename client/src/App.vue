424484fa8e8892986dba6bc512de01b9784db12a
Josh Choi <joshuachoi0508@gmail.com>
2019-06-05 17:12:53 -0700

    Create PoemService methods

---
+++ client/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <PoemComponent />
+  </div>
+</template>
+
+<script>
+import PoemComponent from './components/PoemComponent.vue'
+
+export default {
+  name: 'app',
+  components: {
+    PoemComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
