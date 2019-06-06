ffe67184aa12c6a2efea60a6a4318b3598cfc80c
Josh Choi <joshuachoi0508@gmail.com>
2019-06-05 17:42:16 -0700

    Create main component

--- client/src/components/PoemComponent.vue
+++ client/src/components/PoemComponent.vue
@@ -1,58 +1,92 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <h1>Latest Poems</h1>
+    <div class="create-poem">
+      <label for="create-poem">Say Something...</label>
+      <input type="text" id="create-poems" v-model="text" placeholder="Create a poem">
+      <button v-on:click="createPoem">Post</button>
+    </div>
+    <hr>
+    <p class="error" v-if="error">{{ error }}</p>
+    <div class="poems-container">
+      <div class="poem"
+        v-for="(poem, index) in poems"
+        v-bind:item="poem"
+        v-bind:index="index"
+        v-bind:key="poem._id"
+        v-on:click="deletePoem(poem._id)"
+      >
+        {{ `${poem.createdAt.getDate()}/
+        ${poem.createdAt.getMonth()}/
+        ${poem.createdAt.getFullYear()}` }}
+        <p class="text">{{ poem.text }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import PoemService from '../PoemService'
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'PoemComponent',
+  data() {
+    return {
+      poems: [],
+      error: '',
+      text: ''
+    }
+  },
+  async created() {
+    try {
+      this.poems = await PoemService.getPoems();
+    } catch(err) {
+      this.error = err.message;
+    }
+  },
+  methods: {
+    async createPoem() {
+      await PoemService.insertPoem(this.text);
+      this.poems = await PoemService.getPoems();
+    },
+    async deletePoem(id) {
+      await PoemService.deletePoem(id);
+      this.poems = await PoemService.getPoems();
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+div.container {
+  max-width: 800px;
+  margin: 0 auto;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+p.error{
+  border: 1px solid #ff5b5f;
+  background-color: #ffc5c1;
+  padding: 10px;
+  margin-bottom: 15px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+div.poem{
+  position: relative;
+  border: 1px solid #5bd658;
+  background-color: #bcffb8;
+  padding: 10px 10px 30px 10px;
 }
-a {
-  color: #42b983;
+div.created-at{
+  position: absolute;
+  top:0;
+  left:0;
+  padding: 5px 15px 5px 15px;
+  background-color: darkgreen;
+  color:white;
+  font-size: 13px;
+}
+p.text{
+  font-size: 22px;
+  font-weight: 700;
+  margin-bottom: 0;
 }
 </style>
