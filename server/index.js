b1205a1a64805de1bcbc354938a01e350455ef98
Josh Choi <joshuachoi0508@gmail.com>
2019-06-07 17:31:25 -0700

    .

--- server/index.js
+++ server/index.js
@@ -12,12 +12,12 @@ const poems = require('./routes/api/poems');
 
 app.use('/api/poems', poems);
 
-//Handle production
-if (process.env.NODE_ENV === "production") {
-  //Set Static Folder
-  app.use(express.static(__dirname + './public/'))
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+  // Static folder
+  app.use(express.static(__dirname + '/public/'));
 
-  //Handle SPA
+  // Handle SPA
   app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
