aaff2e744e65efcb49261585022b2446d3a24c51
Josh Choi <joshuachoi0508@gmail.com>
2019-06-09 16:05:35 -0700

    Separate main compoennt and single poem component

--- client/src/components/PoemComponent.vue
+++ client/src/components/PoemComponent.vue
@@ -11,104 +11,27 @@
         v-bind:verbs="verbs"
       />
 
-    <div class="poems-container">
-
-      <div class="poem"
-        v-for="(poem, index) in poems"
-        v-bind:item="poem"
-        v-bind:index="index"
-        v-bind:key="poem._id"
-      >
-        <div class="date-and-delete">
-          <p>
-            {{ poem.createdAt.getDate() }}/{{ poem.createdAt.getMonth() }}/{{ poem.createdAt.getFullYear()}}
-          </p>
-          <div class="edit-and-delete">
-            <p
-              class="edit-button"
-              v-on:click="renderEditForm(poem._id, poem.text)"
-            >Edit</p>
-            <p
-              class="delete-button"
-              v-on:click="deletePoem(poem._id)"
-            >
-              X
-            </p>
-          </div>
-        </div>
-
-          <div 
-            v-if="!(editPoemId === poem._id)"
-            v-html="organizePoem(poem)"
-          >
-          </div>
-
-          <div v-if="editPoemId === poem._id">
-            <form 
-              class="create-poem-form"
-              @submit.prevent="('PUT', poem._id)"
-            >
-              <div class="user-inputs">
-                <label class="user-input-label">
-                  Nouns
-                  <input type="text" class="user-input" v-model="editNouns[0]" placeholder="insert a noun">
-                  <input type="text" class="user-input" v-model="editNouns[1]" placeholder="insert a noun">
-                  <input type="text" class="user-input" v-model="editNouns[2]" placeholder="insert a noun">
-                </label>
-
-                <label class="user-input-label">
-                  Adjectives
-                  <input type="text" class="user-input" v-model="editAdjectives[0]" placeholder="insert a adjective">
-                  <input type="text" class="user-input" v-model="editAdjectives[1]" placeholder="insert a adjective">
-                  <input type="text" class="user-input" v-model="editAdjectives[2]" placeholder="insert a adjective">
-                </label>
-
-                <label class="user-input-label">
-                  Adverbs
-                  <input type="text" class="user-input" v-model="editAdverbs[0]" placeholder="insert a adverb">
-                  <input type="text" class="user-input" v-model="editAdverbs[1]" placeholder="insert a adverb">
-                  <input type="text" class="user-input" v-model="editAdverbs[2]" placeholder="insert a adverb">
-                </label>
-
-                <label class="user-input-label">
-                  Prepositions
-                  <input type="text" class="user-input" v-model="editPrepositions[0]" placeholder="insert a preposition">
-                  <input type="text" class="user-input" v-model="editPrepositions[1]" placeholder="insert a preposition">
-                  <input type="text" class="user-input" v-model="editPrepositions[2]" placeholder="insert a preposition">
-                </label>
-
-                <label class="user-input-label">
-                  Verbs
-                  <input type="text" class="user-input" v-model="editVerbs[0]" placeholder="insert a verb">
-                  <input type="text" class="user-input" v-model="editVerbs[1]" placeholder="insert a verb">
-                  <input type="text" class="user-input" v-model="editVerbs[2]" placeholder="insert a verb">
-                </label>
-              </div>
-
-              <p v-if="editErrors.length">
-                <ul class="error-list">
-                  <li class="error"
-                    v-for="error in editErrors"
-                    v-bind:item="error"
-                    v-bind:key="error"
-                  >{{ error }}</li>
-                </ul>
-              </p>
-
-              <button class="submit-button" type="submit">Submit</button>
-            </form>
-          </div>
-
-      </div>
-
-    </div>
-
+      <SinglePoemComponent 
+        v-bind:handleSubmit="handleSubmit"
+        v-bind:editErrors="editErrors"
+        v-bind:editNouns="editNouns"
+        v-bind:editAdjectives="editAdjectives"
+        v-bind:editAdverbs="editAdverbs"
+        v-bind:editPrepositions="editPrepositions"
+        v-bind:editVerbs="editVerbs"
+        v-bind:editPoemId="editPoemId"
+        v-bind:poems="poems"
+        v-bind:renderEditForm="renderEditForm"
+        v-bind:organizePoem="organizePoem"
+      />
+      
   </div>
 </template>
 
 <script>
 import PoemService from '../PoemService'
-import CreatePoemComponent from './CreatePoemComponent'
+import CreatePoemComponent from './CreatePoemComponent';
+import SinglePoemComponent from './SinglePoemComponent';
 
 export default {
   name: 'PoemComponent',
@@ -133,7 +56,8 @@ export default {
     }
   },
   components: {
-    CreatePoemComponent
+    CreatePoemComponent,
+    SinglePoemComponent
   },
   async created() {
     try {
@@ -145,10 +69,6 @@ export default {
   methods: {
     async createPoem() {
       await PoemService.insertPoem(this.text);
-      this.poems = await PoemService.getPoems();
-    },
-    async deletePoem(id) {
-      await PoemService.deletePoem(id);
       this.poems = await PoemService.getPoems();
     },
     async editPoem(id) {
