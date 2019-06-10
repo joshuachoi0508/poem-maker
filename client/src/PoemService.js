9b30f1bed64fc39d88462720b001ee60adbaba24
Josh Choi <joshuachoi0508@gmail.com>
2019-06-10 14:18:34 -0700

    Revert to c58fb1e7118e28a983a76dfc05cb2239d1cb3654

---
+++ client/src/PoemService.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+const url = 'api/poems/'
+
+class PoemService {
+  //Get Poems
+  static getPoems() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const res = await axios.get(url);
+        const data = res.data;
+        resolve(
+          data.map(poem => ({
+            ...poem,
+            createdAt: new Date(poem.createdAt)
+          }))
+        );
+      } catch(err) {
+        reject(err);
+      }
+    });
+  }
+
+  //Create Poems
+  static insertPoem(text) {
+    return axios.post(url, {
+      text
+    });
+  }
+
+  //Edit Poems
+  static editPoem(id, text) {
+    return axios.put(`${url}${id}`, {
+      text
+    });
+  }
+
+  //Delete Poems
+  static deletePoem(id) {
+    return axios.delete(`${url}${id}`)
+  }
+}
+
+export default PoemService;
